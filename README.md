815977109cf2accba446c3ba6fad916814fca026
dhaval7b <dhavalrbhimani@gmail.com>
2024-05-08 15:29:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+
+
+## Requirements
+
+```
+- Git & Github Account
+- NPM (Node Package Manager)
+- A text-editor
+```
+
+## Installation
+
+After cloning and setting up the project files on your local machine, run the following command in your terminal
+
+```bash
+npm install
+```
+
+Once the packages are installed you are ready to run astro. Astro comes with a built-in development server that has everything you need for project development. The astro dev command will start the local development server so that you can see your new website in action for the very first time.
+
+```bash
+npm run dev
+```
+
+## Tech Stack
+
+- [Astro](https://astro.build)
+- [TailwindCSS](https://tailwindcss.com/)
+- [AlpineJs](https://alpinejs.dev/)
+
+## Project Structure
+
+```php
+├── src/
+│   ├── components/
+│   │   ├── Card.astro
+│   │   ├── Container.astro
+│   │   ├── Footer.astro
+│   │   └── Header.astro
+│   ├── layouts/
+│   │   └── AccordionLayout.astro
+│   │   └── BaseLayout.astro
+│   └── pages/
+│   │   ├── content.js
+│   │   └── index.astro
+├── public/
+│   ├── favicon.svg
+│   └── profile.jpg
+│   └── screenshot.jpeg
+├── astro.config.mjs
+├── tailwind.config.cjs
+├── package.json
+├── README.md
+├── .gitignore
+└── tsconfig.json
+```
+
+## Using & Editing the content
+
+In order to change the content of the CV, all you have to do is update it in the `content.js` file in `/src` folder.
+
+![Content Code](public/content_code.png)
+
+In `src/content.js` all variables contain information that is to be reflected in the UI. In order to change it, simply update the values with your data. The personal information is fairly simple, content for the likes of work, projects, etc are stored as JavaScript objects.
+
+#### Adding or Removing a field
+
+To add or remove a field from the CV, simply go to `Container.astro` in `src/components/`
+
+![Accordion_code](public/accordion_code.png)
+
+- To Add a new field, add a new `<AccordionLayout />` component, with appropriate props and child component. Make sure add the data of field in proper format in `content.js`.
+- To Remove an already existing field, simply comment out the `<AccordionLayout />` component of that particular field or remove it. Removing its content from `content.js` is a choice.
---
+++ src/pages/content.js
@@ -0,0 +1,135 @@
+// Personal Information --> #hero section
+export const name = "Dhaval Bhimani"
+export const designation = "Full Stack Software Developer"
+export const location = "Toronto,Canada"
+
+// About
+export const about = "A Software developer with 3+ years of experience in building highly scalable backend applications(Java, Node.js, C#) in microservice architecture and SPA frontend web applications(React, Angular)";
+
+// Work Experience --> #work section
+export const work = [
+	{
+		from: "September 2023",
+		to: "Present",
+		role: "Software Developer",
+		org: "Intuit Inc",
+		org_url: "https://www.intuit.com/ca/",
+		location: "Toronto, Canada",
+		skills: "Java, C#, React"
+	},
+	{
+		from: "May 2021",
+		to: "September 2023",
+		role: "Software Developer",
+		org: "Rogers communications Inc",
+		org_url: "https://www.rogers.com/",
+		location: "Toronto, Canada",
+		skills: "Java, C#, Angular"
+	},
+	{
+		from: "November 2017",
+		to: "August 2018",
+		role: "Software Developer",
+		org: "Pixelbot",
+		org_url: "https://www.hellopixelbot.com/",
+		location: "Toronto, Canada",
+		skills: "JavaScript, Node.js, React"
+	}
+]
+
+// Projects --> #project section
+export const projects = [
+	{
+		from: "Sep 2023",
+		to: "Present",
+		title: "Pro Tax- QuickBooks",
+		description: "",
+		url: "https://quickbooks.intuit.com/ca/accountants/features/tax-returns/",
+		tech: "",
+	},
+	{
+		from: "May 2021",
+		to: "Sep 2023",
+		title: "Oneview",
+		description: "",
+		url: "https://www.rogers.com/",
+		tech: "",
+	},
+	{
+		from: "2021",
+		to: "2023",
+		title: "Change Bill Cycle Microservice",
+		description: "",
+		url: "https://www.rogers.com/",
+		tech: "",
+	},
+	{
+		from: "2021",
+		to: "2023",
+		title: "Promise to Pay microservice",
+		description: "",
+		url: "https://www.rogers.com/support/billing-accounts/payment-arrangement",
+		tech: "",
+	},
+	{
+		from: "2022",
+		to: "2022",
+		title: "RBI",
+		description: "",
+		url: "https://www.rogers.com/",
+		tech: "",
+	}
+]
+
+// Education --> #education section
+export const education = [
+	{
+		from: "2017",
+		to: "2019",
+		institute: "Centennial college",
+		course: "Postgraduate Diploma",
+		location: "Toronto, Canada"
+	},
+	{
+		from: "2011",
+		to: "2016",
+		institute: "Gujarat Technological University",
+		course: "Bachelor of Engineering",
+		location: "Ahemdabad, Gujarat"
+	}
+]
+
+// Contact --> #contact section
+// export const contact = [
+// 	{
+// 		source_name: "Email",
+// 		source: "dhavalrbhimani.com",
+// 	},
+// 	{
+// 		source_name: "Github",
+// 		source: "",
+// 	},
+// 	{
+// 		source_name: "LinkedIn",
+// 		source: "https://www.linkedin.com/in/",
+// 	},
+// ]
+
+// Certifications --> #certificate section
+
+export const certificates = [
+	{
+		issued: "2022",
+		title: "AWS Certified Cloud Practitioner",
+		org: "AWS",
+		url: "https://www.credly.com/badges/20517025-0f62-4e8d-9401-94602397b4e6/linked_in_profile",
+		description: "Initial certification in AWS cloud",
+	},
+	{
+		issued: "2022",
+		title: "Microsoft Certified: Azure Fundamentals",
+		org: "Microsoft",
+		url: "https://www.credly.com/badges/1f729ee2-ee92-479a-be0b-d23fa7e900ef/linked_in_profile",
+		description: "Initial certification in Microsoft Azure cloud",
+	}
+]
